16bc8d12ba6bd5bbaa25f1517d83843476c25d9c
kellogsricekrispy <79350471+kellogsricekrispy@users.noreply.github.com>
2022-04-12 22:43:36 -0500

    attempting to make draggable

--- root/script.js
+++ root/script.js
@@ -1,8 +1,44 @@
 $( document ).ready(function() {
 
-    ()=>{
-        
-    }
+    const folder = document.querySelector('.folder');
+    let dragValue;
+
+    // listeners 
+    window.addEventListener('load', move);
+    
+    folder.addEventListener('dragstart', dragStart);
+    folder.addEventListener('dragend', dragEnd);
+
+    // body.addEventListener('mousedown', drag)  
+   function dragStart(){
+       setTimeout(()=> (this.className="invisible"),0);
+   }
    
     console.log( "ready!" );
+
+    function dragEnd(){
+        
+    }
+
+    function move(id){
+        let element = document.getElementById('process-folder');
+        element.style.position = "absolute";
+        element.onmousedown = function(){
+            dragValue = element;
+        }
+    }
+
+    document.addEventListener('mousemove',
+        function(e){
+            let x = e.pageX;
+            let y = e.pageY;
+
+            dragValue.style.left = x+"px";
+            dragValue.style.top = y+"px";
+
+        }    
+    )
+    
 });
+
+   
