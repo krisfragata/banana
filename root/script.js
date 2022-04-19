1ab28c733a88bb7a96a32e66cf106881b12c963f
kellogsricekrispy <79350471+kellogsricekrispy@users.noreply.github.com>
2022-04-19 18:14:23 -0500

    adding close open functionality

--- root/script.js
+++ root/script.js
@@ -1,29 +1,49 @@
 $( document ).ready(function() {
 
-    const folder = document.querySelector('.folder');
-    let dragValue;
+    // const folder = document.querySelector('.folder');
+    const promo = document.querySelector('#promo-folder');
+    const musicV = document.querySelector('#mv-folder');
+    let lastClicked;
+    // let dragValue;
 
     // listeners 
-    // window.addEventListener('load', move);
-    window.addEventListener('click', console.log('click'));
+    // window.addEventListener('click', console.log('click'));
+    promo.addEventListener('click', (e)=>{
+        const folder = document.querySelector('#promo-container')
+        lastClicked = folder;
+        e.preventDefault();
+        console.log('promo clicked');
+        isItHidden(folder);
+    });
+
+    musicV.addEventListener('click', ()=>{
+        const folder = document.querySelector('#mv-popUp');
+        lastClicked = folder;
+        isItHidden(folder);
+    })
+
+
+    $('.close').click(()=>{
+        console.log('closed clicked');
+        // const folder = document.querySelector('#promo-container');
+        isItHidden(lastClicked);
+    })
+    
     
     // folder.addEventListener('dragstart', dragStart);
     // folder.addEventListener('dragend', dragEnd);
 
 
-    function hide(){
+    function isItHidden(folder){
         console.log('click');
-        let isHidden = false;
-        $(".close").click(()=>{
-            if(isHidden == true){
-                $(".container").removeClass('hidden');
-                isHidden = false;
-            }
-            else{
-                $('.close').addClass('hidden')
-                isHidden = true;
-            }
-        })
+        if(folder.classList.contains('hidden')){
+            console.log('is hidden')
+            folder.classList.remove('hidden');
+        }
+        else{
+            folder.classList.add('hidden');
+            console.log('is not hidden');
+        }
     };
     // body.addEventListener('mousedown', drag)  
 //    function dragStart(){
