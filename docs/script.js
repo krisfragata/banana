84f26c8326b58c121573a480c9f1b8cb9d1bf960
kellogsricekrispy <79350471+kellogsricekrispy@users.noreply.github.com>
2022-04-29 00:11:05 -0500

    things

--- docs/script.js
+++ docs/script.js
@@ -153,23 +153,23 @@ $( document ).ready(function() {
     })
 
     // text animations
-    let tl = gsap.timeline(), 
+    // let tl = gsap.timeline(), 
 
-    split = new SplitText("#quote", {type:"words,chars"}), 
+    // split = new SplitText("#quote", {type:"words,chars"}), 
 
-    words = split.words; //an array of all the divs that wrap each character
+    // words = split.words; //an array of all the divs that wrap each character
 
-    gsap.set(".about", {perspective: 400});
+    // gsap.set(".about", {perspective: 400});
 
-    tl.to(words, {
-      duration: 1.5,
-      "--weight": "300",
-      ease: "none",
-      color: "hsl(+=0, +=70%, +=20%)",
-      stagger: {
-        each: 0.4,
-      }
-    })
+    // tl.to(words, {
+    //   duration: 1.5,
+    //   "--weight": "300",
+    //   ease: "none",
+    //   color: "hsl(+=0, +=70%, +=20%)",
+    //   stagger: {
+    //     each: 0.4,
+    //   }
+    // })
 
 
     function closeIt(folder){
